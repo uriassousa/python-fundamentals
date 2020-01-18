9fa8f432f7ae286eee08a4e6a46a71c45d121e25
urias Urias <uriassousa@hotmail.com>
2020-01-18 16:26:43 -0200

    Upgrade files pos coffee

--- aulas/basic.py
+++ aulas/basic.py
@@ -22,20 +22,42 @@
 # Comentarios
 
     # - comentario de linha unica
-print('Curso Python Fundamentals')
-print(100)
-print(1.5)
+# print('Curso Python Fundamentals')
+# print(100)
+# print(1.5)
 
 # Variaveis
 
-nome = 'Urias'
-idade = 47
-peso = 87.7
+# nome = 'Urias'
+# idade = 47
+# peso = 87.7
 
-print(nome)
+# print(nome)
 
 # Entrada de dados
 
-usuario = input('Digite seu nome: ')
+# usuario = input('Digite seu nome: ')
 
-print(usuario)
+# print(usuario)
+
+# n1 = int(input('Digite o primeiro numero: '))
+# n2 = int(input('Digite o segundo numero: '))
+
+# print(n1 + n2)
+
+# Façca um programa que receba 4 notas de alunoas
+# Some e divida por 4
+
+# aluno1 = float(input('digite a nota do aluno1: '))
+# aluno2 = float(input('digite a nota do aluno2: '))
+# aluno3 = float(input('digite a nota do aluno3: '))
+# aluno4 = float(input('digite a nota do aluno4: '))
+
+# nota_final = (aluno1 + aluno2 + aluno3 + aluno4) / 4
+
+# print(nota_final)
+
+nome = input ('Digite o nome: ')
+sobrenome = input ('Digite o sobrenome: ')
+
+print(f' O nome é: {nome} e o sobrenome é: {sobrenome}')
