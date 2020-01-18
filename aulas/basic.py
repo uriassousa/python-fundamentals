8190b18f0ed805e63b43e79596595152ed8caf5e
urias Urias <uriassousa@hotmail.com>
2020-01-18 15:15:30 -0200

    Update file basic.py v0.2

--- aulas/basic.py
+++ aulas/basic.py
@@ -22,5 +22,20 @@
 # Comentarios
 
     # - comentario de linha unica
-
 print('Curso Python Fundamentals')
+print(100)
+print(1.5)
+
+# Variaveis
+
+nome = 'Urias'
+idade = 47
+peso = 87.7
+
+print(nome)
+
+# Entrada de dados
+
+usuario = input('Digite seu nome: ')
+
+print(usuario)
