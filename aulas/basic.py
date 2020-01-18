59f151f5769cc5648e4ea27eefdad6747b72e861
urias Urias <uriassousa@hotmail.com>
2020-01-18 15:14:31 -0200

    Alteracao arquivo basic.py - v0.1

--- aulas/basic.py
+++ aulas/basic.py
@@ -1,1 +1,26 @@
- 
+ # Tipos de dados
+
+    # inteiro
+
+        # 1
+        # 10
+        # 20
+        #540
+    # float
+
+        # 1.0
+        # 1.5
+        # 20.6
+        # 5.0
+
+    # String
+
+    # 'Renato'
+    # 'Python Fundamentals'
+    # '100'
+
+# Comentarios
+
+    # - comentario de linha unica
+
+print('Curso Python Fundamentals')
