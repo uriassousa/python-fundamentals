5a82a5b8ee24ae4275051b9c71b40e7685754a30
Urias Sousa <uriassousa@hotmail.com>
2020-01-27 22:43:55 -0300

    Ex guanabara

--- aulas/guanabara/ex004.1.py
+++ aulas/guanabara/ex004.1.py
@@ -1,7 +1,6 @@
 n1 = int(input('Entre com primeiro numero: '))
 n2 = int(input('Entre com o segundo numero: '))
 
-
 soma = n1 + n2
 
-print('A soma dos número é: ' , soma)
+print('A soma entre {} é {} é igual a {}!'.format(n1, n2, soma))
