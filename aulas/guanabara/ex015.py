a858105698bc8626e3cf79d4b1c49fdab0eacf18
Urias Sousa <uriassousa@hotmail.com>
2020-02-03 12:27:17 -0300

    Test1 03-02-2020

--- aulas/guanabara/ex015.py
+++ aulas/guanabara/ex015.py
@@ -1,8 +1,8 @@
 #Aluguel de carros
 dias = float(input('Quantidade de dias para o aluguel: '))
 valor = dias * 60 
-kmrodado = float(input('Digite o valor dos KMs rodados: '))
+kmrodado = float(input('Quantidade de KMs rodados: '))
 km = kmrodado * 0.15
 total = valor + km
-print(' O valor a pagar é R${}, somando os R${} dos KM´s rodados o valor final a ser pago é R${} '.format(valor, km, total))
+print(' O valor somando os dias é R${:.2f}, mais os R${:.2f} dos KM´s rodados, o valor final a ser pago é R${:.2f} '.format(valor, km, total))
 
---
+++ aulas/guanabara/test1.py
@@ -0,0 +1,4 @@
+salb = float(input('Digite o salário bruto: '))
+sind = salb * 3 / 100
+
+if <= 1900
